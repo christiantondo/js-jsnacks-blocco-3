f19257991fb91b92f7eb677b0c5c477775f78b8e
christiantondo <christiantondo1@gmail.com>
2025-11-22 15:40:19 +0100

    snack4 array created

--- snack4/main.js
+++ snack4/main.js
@@ -1,4 +1,13 @@
 // Crea un array di oggetti che rappresentano delle persone.
 // Ogni persona ha un nome, un cognome e un’età.
 
+const people = [
+    {name:"Alessio", surname:"Mattei", age:"24"},
+    {name:"Matteo", surname:"Sabatini", age:"25"},
+    {name:"Simone", surname:"Angelini", age:"30"},
+    {name:"Enrico", surname:"Mieli", age:"49"},
+    {name:"Giovanni", surname:"Tondo", age:"52"},
+    {name:"Giuseppe", surname:"Mieli", age:"81"}
+]
+
 // Partendo da questo, crea quindi un nuovo array inserendo, per ogni persona, una frase (stringa) con il nome e cognome e l’indicazione se può guidare, in base all’età.
