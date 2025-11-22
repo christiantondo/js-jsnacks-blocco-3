c6bdcc5fe7f0562519a470f43ea7c75847987920
christiantondo <christiantondo1@gmail.com>
2025-11-22 10:29:50 +0100

    snack2 completed

--- snack2/main.js
+++ snack2/main.js
@@ -1,3 +1,8 @@
 //A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
-
 // Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+const names = ["pippo", "PLUTO", "Paperino"];
+const newNames = names.map(name => {
+    const namelowerCase = name.toLowerCase();
+    return namelowerCase.charAt(0).toUpperCase() + namelowerCase.slice(1);
+})
