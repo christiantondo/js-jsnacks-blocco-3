b6af0195f6d54f53c5cf6dca0312f891063dbe41
christiantondo <christiantondo1@gmail.com>
2025-11-22 16:40:20 +0100

    array edit and basic function

--- snack4/main.js
+++ snack4/main.js
@@ -2,12 +2,21 @@
 // Ogni persona ha un nome, un cognome e un’età.
 
 const people = [
-    {name:"Alessio", surname:"Mattei", age:"24"},
-    {name:"Matteo", surname:"Sabatini", age:"25"},
-    {name:"Simone", surname:"Angelini", age:"30"},
-    {name:"Enrico", surname:"Mieli", age:"49"},
-    {name:"Giovanni", surname:"Tondo", age:"52"},
-    {name:"Giuseppe", surname:"Mieli", age:"81"}
+    { name: "Alessio", surname: "Mattei", age: "8" },
+    { name: "Matteo", surname: "Sabatini", age: "25" },
+    { name: "Simone", surname: "Angelini", age: "30" },
+    { name: "Enrico", surname: "Mieli", age: "12" },
+    { name: "Giovanni", surname: "Tondo", age: "52" },
+    { name: "Giuseppe", surname: "Mieli", age: "81" }
 ]
 
 // Partendo da questo, crea quindi un nuovo array inserendo, per ogni persona, una frase (stringa) con il nome e cognome e l’indicazione se può guidare, in base all’età.
+
+// Nuovo array con all'interno
+// const namePlusSurname = people.map(person => person.name + " " + person.surname)
+
+function canTheyDrive() {
+    const namePlusSurname = people.map(person => `${person.name} ${person.surname}`)
+    const canTheyDrive = person.age >= 18
+}
+// Nuovo array con dentro ogni persona e una stringa con nome e cognome
