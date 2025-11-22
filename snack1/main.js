b25268c369fae2357f3d7afba8e015533f59ab7e
christiantondo <christiantondo1@gmail.com>
2025-11-22 09:48:56 +0100

    snack1 array of object created

--- snack1/main.js
+++ snack1/main.js
@@ -1,6 +1,18 @@
 // Crea un array composto da 10 automobili.
 // Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
 
+const cars = [
+    { brand: "Mclaren", model: "Senna", fueltype: "petrol"},
+    { brand: "Lexus", model: "LFA", fueltype: "Petrol"},
+    { brand: "Ford", model: "Focus", fueltype: "Diesel"},
+    { brand: "Volvo", model: "XC90", fueltype: "Diesel"},
+    { brand: "Dacia", model: "Sandero", fueltype: "Gpl"},
+    { brand: "Fiat", model: "Panda", fueltype: "Gpl"},
+    { brand: "Porsche", model: "Taycan", fueltype: "Elettrico"},
+    { brand: "Audi", model: "e-tron GT", fueltype: "Elettrico"},
+    { brand: "Volkswagen", model: "eco up", fueltype: "Metano"},
+    { brand: "Seat", model: "Ibiza", fueltype: "Metano"}
+]
 
 
 // Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
